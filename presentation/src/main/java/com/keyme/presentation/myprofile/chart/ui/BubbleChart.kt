b5179bbf579aed3269bb1e8091d149a5e9c32a1a
DwEnn <prgohn@gmail.com>
2023-08-10 01:51:04 +0900

    bubble item 구현

--- presentation/src/main/java/com/keyme/presentation/myprofile/chart/ui/BubbleChart.kt
+++ presentation/src/main/java/com/keyme/presentation/myprofile/chart/ui/BubbleChart.kt
@@ -1,5 +1,7 @@
 package com.keyme.presentation.myprofile.chart.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +10,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.UiComposable
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.zIndex
 import com.keyme.domain.entity.response.Circle
+import com.keyme.presentation.designsystem.theme.keyme_black
 import com.keyme.presentation.myprofile.chart.rememberBubbleChartState
 
 @Composable
@@ -22,7 +27,18 @@ fun BubbleChart(circles: List<Circle>) {
             ),
         )
 
-        BubbleChartCanvas(bubbleChartState = bubbleChartState)
+        if (bubbleChartState.bubbleChartInitialState.isInit()) bubbleChartState.init()
+
+        if (bubbleChartState.bubbleChartInitialState.isFinish()) {
+            BubbleChartCanvas(bubbleChartState = bubbleChartState)
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = keyme_black)
+                    .zIndex(1f),
+            )
+        }
     }
 }
 
@@ -36,93 +52,3 @@ private fun BubbleChartContainer(
         content = content,
     )
 }
-
-
-//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-//@Composable
-//fun DrawSample() {
-//    val result = circleDummy()
-//    var size by remember {
-//        mutableStateOf(IntSize.Zero)
-//    }
-//    val scale = size.width
-//
-//    val colors = listOf(
-//        Color.Blue,
-//        Color.Green,
-//        Color.Gray,
-//        Color.Red,
-//        Color.LightGray,
-//        Color.Green,
-//        Color.Cyan,
-//        Color.Yellow,
-//        Color.Red,
-//        Color.LightGray,
-//    )
-//
-//    var offset by remember { mutableStateOf(Offset.Zero) }
-//    val state = rememberTransformableState { zoomChange, offsetChange, _ ->
-//        offset += offsetChange
-//    }
-//
-//    val rects = mutableListOf<Rect>()
-//    val rr = rememberUpdatedState(newValue = rects)
-//
-//    val image = ImageVector.vectorResource(id = com.keyme.presentation.R.drawable.icon_camera)
-//
-//    var center by remember {
-//        mutableStateOf(Offset.Zero)
-//    }
-//
-//    val context = LocalContext.current
-//
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .background(color = Color.Black)
-//            .onGloballyPositioned {
-//                size = it.size
-//                center = it.boundsInWindow().center
-//            }
-//            .transformable(state)
-//            .graphicsLayer(
-//                translationX = offset.x,
-//                translationY = offset.y,
-//            ),
-//    ) {
-//        Canvas(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .graphicsLayer(
-//                    translationX = offset.x,
-//                    translationY = offset.y,
-//                )
-//                .pointerInput(Unit) {
-//                    detectTapGestures(
-//                        onTap = { tapOffset ->
-//                            rr.value
-//                                .find { it.contains(tapOffset) }
-//                                ?.let {
-//                                    Log.d("><>", it.toString())
-//                                }
-//                        },
-//                    )
-//                },
-//            onDraw = {
-//                rects.clear()
-//                result.forEachIndexed { index, circle ->
-//                    drawResultBubble(circle, colors[index], scale, "")
-//                }
-//            },
-//        )
-//
-//        rects.forEach {
-//            Log.d("><>", it.center.toString())
-//            Column {
-//                Text(text = "aasdf", modifier = Modifier.offset(it.center.x.dp, it.center.y.dp))
-//            }
-//        }
-//    }
-//}
-
-
--- presentation/src/main/java/com/keyme/presentation/myprofile/chart/ui/BubbleChartCanvas.kt
+++ presentation/src/main/java/com/keyme/presentation/myprofile/chart/ui/BubbleChartCanvas.kt
@@ -1,19 +1,31 @@
 package com.keyme.presentation.myprofile.chart.ui
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.drawText
-import com.keyme.domain.entity.response.Circle
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.keyme.presentation.myprofile.chart.BubbleChartState
-import com.keyme.presentation.utils.scale
 
 @Composable
 fun BubbleChartCanvas(
@@ -22,13 +34,12 @@ fun BubbleChartCanvas(
     Canvas(
         modifier = Modifier.bubbleChart(bubbleChartState),
         onDraw = {
-            bubbleChartState.bubbleRectList.forEachIndexed { index, rect ->
-                drawResultBubble(
-                    bubbleChartState,
-                    rect,
-                    colors[index],
-                    "",
-                )
+            repeat(bubbleChartState.bubbleRectList.size) {index ->
+                val rect = bubbleChartState.bubbleRectList[index]
+                val itemBitmap = bubbleChartState.bubbleChartItemBitmaps[index]
+
+                drawResultBubble(rect, colors[index])
+                drawResultItem(rect, itemBitmap)
             }
         },
     )
@@ -48,22 +59,17 @@ private val colors = listOf(
 )
 
 private fun DrawScope.drawResultBubble(
-    bubbleChartState: BubbleChartState,
     bubbleRect: Rect,
     color: Color,
-    info: String,
 ) {
-    val circlePath = Path().apply {
-        addOval(bubbleRect)
-    }
-
+    val circlePath = Path().apply { addOval(bubbleRect) }
     drawPath(circlePath, color)
 }
 
-@OptIn(ExperimentalTextApi::class)
-private fun DrawScope.drawResultInfo(textMeasurer: TextMeasurer, offset: Offset, info: String) {
-    drawText(
-        textMeasurer = textMeasurer,
-        text = info,
-    )
+private fun DrawScope.drawResultItem(bubbleRect: Rect, item: Bitmap) {
+    val targetX = bubbleRect.center.x - (item.width / 2)
+    val targetY = bubbleRect.center.y - (item.height / 2)
+    val targetOffset = Offset(targetX, targetY)
+
+    drawImage(image = item.asImageBitmap(), topLeft = targetOffset)
 }

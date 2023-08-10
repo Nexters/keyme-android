1fb58e551bbe62561610c689eea312a67e4a47e8
DwEnn <prgohn@gmail.com>
2023-08-10 23:45:13 +0900

    typo 추가

--- presentation/src/main/java/com/keyme/presentation/designsystem/theme/Type.kt
+++ presentation/src/main/java/com/keyme/presentation/designsystem/theme/Type.kt
@@ -2,9 +2,12 @@ package com.keyme.presentation.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.keyme.presentation.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,4 +18,14 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
     ),
+)
+
+val panchang = FontFamily(
+    Font(R.font.panchang_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.panchang_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.panchang_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.panchang_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.panchang_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.panchang_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.panchang_semibold, FontWeight.SemiBold, FontStyle.Normal),
 )

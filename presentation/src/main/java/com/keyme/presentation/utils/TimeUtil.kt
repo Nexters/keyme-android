95c721569cdd3b7f442ecc1dc2125e6c8474863f
DwEnn <prgohn@gmail.com>
2023-08-12 20:27:48 +0900

    getUploadTimeStamp 구현

---
+++ presentation/src/main/java/com/keyme/presentation/utils/TimeUtil.kt
@@ -0,0 +1,23 @@
+package com.keyme.presentation.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+fun Long.getUploadTimeStamp(): String {
+    val uploadInterval = System.currentTimeMillis() - this
+
+    val minutesUnit = TimeUnit.MINUTES.toMillis(1)
+    val hourUnit = TimeUnit.HOURS.toMillis(1)
+    val dayUnit = TimeUnit.DAYS.toMillis(1)
+
+    val format = SimpleDateFormat("aa hh:mm")
+
+    return when (uploadInterval) {
+        in 0L until minutesUnit -> "방금 전"
+        in minutesUnit until hourUnit -> "${uploadInterval / minutesUnit}분 전"
+        in hourUnit until dayUnit -> "${uploadInterval / hourUnit}시간 전"
+        in dayUnit until dayUnit * 2 -> "어제 ${format.format(Date(this))}"
+        else -> "엊그제"
+    }
+}

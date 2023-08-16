f77f9ad27deb961da234deeafce387029004dcd0
DwEnn <prgohn@gmail.com>
2023-08-16 20:33:12 +0900

    미사용 클래스 제거

--- presentation/src/main/java/com/keyme/presentation/feed/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.keyme.presentation.feed
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()

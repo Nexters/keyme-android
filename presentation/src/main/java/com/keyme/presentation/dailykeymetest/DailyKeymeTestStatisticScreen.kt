7875252f02caaf72a27ad45e410210f14ff1d086
DwEnn <prgohn@gmail.com>
2023-09-23 17:15:21 +0900

    홈 리스트 화면 디자인 수정

--- presentation/src/main/java/com/keyme/presentation/dailykeymetest/DailyKeymeTestStatisticScreen.kt
+++ presentation/src/main/java/com/keyme/presentation/dailykeymetest/DailyKeymeTestStatisticScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,11 +21,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.keyme.domain.entity.member.Member
@@ -33,8 +36,11 @@ import com.keyme.domain.entity.response.TestStatistic
 import com.keyme.presentation.R
 import com.keyme.presentation.designsystem.component.KeymeText
 import com.keyme.presentation.designsystem.component.KeymeTextType
+import com.keyme.presentation.designsystem.theme.keyme_black
 import com.keyme.presentation.utils.ColorUtil
 import com.keyme.presentation.utils.clickableRippleEffect
+
+private val bottomGradientHeightDp = 125.dp
 
 @Composable
 fun DailyKeymeTestStatisticScreen(
@@ -43,74 +49,81 @@ fun DailyKeymeTestStatisticScreen(
     onShareClick: () -> Unit,
     onQuestionStatisticClick: (QuestionStatistic) -> Unit,
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        StatisticTextTitle(myCharacter = myCharacter)
-        Spacer(modifier = Modifier.height(96.dp))
-        StatisticList(
-            modifier = Modifier.weight(1f),
-            myCharacter,
-            dailyKeymeTestStatistic,
-            onQuestionStatisticClick,
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+        ) {
+            item {
+                StatisticTextTitle()
+            }
+            item {
+                StatisticSolvedCountInfo(dailyKeymeTestStatistic)
+            }
+
+            statisticList(
+                myCharacter,
+                dailyKeymeTestStatistic,
+                onQuestionStatisticClick,
+            )
+
+            item { Spacer(modifier = Modifier.height(bottomGradientHeightDp)) }
+        }
+
+        ShareToFriendsButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .zIndex(1f),
+            onClick = onShareClick,
         )
-        ShareToFriendsButton(onClick = onShareClick)
     }
 }
 
 @Composable
-private fun StatisticTextTitle(modifier: Modifier = Modifier, myCharacter: Member) {
+private fun StatisticTextTitle(modifier: Modifier = Modifier) {
     KeymeText(
         modifier = modifier
-            .padding(vertical = 75.dp, horizontal = 16.dp)
+            .padding(top = 75.dp, bottom = 24.dp)
             .fillMaxWidth(),
-        text = "${myCharacter.nickname}님 친구들의 답변이 쌓이고 있어요!",
+        text = "친구들의\n답변이 쌓이고 있어요!",
         keymeTextType = KeymeTextType.HEADING_1,
         color = Color(0xFFF8F8F8),
     )
 }
 
 @Composable
-private fun StatisticList(
-    modifier: Modifier = Modifier,
+private fun StatisticSolvedCountInfo(dailyKeymeTestStatistic: TestStatistic) {
+    Row(
+        modifier = Modifier.padding(bottom = 33.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.icon_member),
+            contentDescription = "",
+            tint = Color.White,
+        )
+
+        KeymeText(
+            text = "${dailyKeymeTestStatistic.solvedCount}명의 친구가 문제를 풀었어요",
+            keymeTextType = KeymeTextType.BODY_4,
+            color = Color.White,
+        )
+    }
+}
+
+private fun LazyListScope.statisticList(
     myCharacter: Member,
     dailyKeymeTestStatistic: TestStatistic,
     onClickItem: (QuestionStatistic) -> Unit,
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.icon_member),
-                contentDescription = "",
-                tint = Color.White,
-            )
-
-            KeymeText(
-                text = "${dailyKeymeTestStatistic.solvedCount}명의 친구가 문제를 풀었어요",
-                keymeTextType = KeymeTextType.BODY_4,
-                color = Color.White,
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-        ) {
-            items(dailyKeymeTestStatistic.questionsStatistics) {
-                QuestionStatisticItem(
-                    myCharacter = myCharacter,
-                    statistic = it,
-                    onClick = { onClickItem(it) },
-                )
-            }
-        }
+    items(dailyKeymeTestStatistic.questionsStatistics) {
+        QuestionStatisticItem(
+            myCharacter = myCharacter,
+            statistic = it,
+            onClick = { onClickItem(it) },
+        )
     }
 }
 
@@ -123,6 +136,7 @@ private fun QuestionStatisticItem(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(bottom = 16.dp)
             .background(color = Color(0x0DFFFFFF), shape = RoundedCornerShape(size = 14.dp))
             .padding(horizontal = 14.dp, vertical = 20.dp)
             .clickableRippleEffect { onClick() },
@@ -140,9 +154,7 @@ private fun QuestionStatisticItem(
         ) {
             AsyncImage(
                 modifier = Modifier.size(20.dp),
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(statistic.category.iconUrl)
-                    .build(),
+                model = ImageRequest.Builder(LocalContext.current).data(statistic.category.iconUrl).build(),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
             )
@@ -150,7 +162,7 @@ private fun QuestionStatisticItem(
 
         Column(verticalArrangement = Arrangement.spacedBy(6.dp)) {
             KeymeText(
-                text = "${myCharacter.nickname}님의 ${statistic.keyword} 정도는?",
+                text = myCharacter.nickname + statistic.title,
                 keymeTextType = KeymeTextType.BODY_3_SEMIBOLD,
                 color = Color.White,
             )
@@ -165,20 +177,33 @@ private fun QuestionStatisticItem(
 }
 
 @Composable
-private fun ShareToFriendsButton(onClick: () -> Unit) {
+private fun ShareToFriendsButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
     Box(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 25.dp)
+        modifier = modifier
             .fillMaxWidth()
-            .height(60.dp)
-            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 16.dp))
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center,
+            .height(bottomGradientHeightDp)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color.Transparent, keyme_black),
+                    endY = 100f,
+                ),
+            ),
+        contentAlignment = Alignment.BottomCenter,
     ) {
-        KeymeText(
-            text = "친구에게 공유하기",
-            keymeTextType = KeymeTextType.BODY_2,
-            color = Color.Black,
-        )
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 25.dp)
+                .fillMaxWidth()
+                .height(60.dp)
+                .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 16.dp))
+                .clickable { onClick() },
+            contentAlignment = Alignment.Center,
+        ) {
+            KeymeText(
+                text = "테스트 공유하기",
+                keymeTextType = KeymeTextType.BODY_2,
+                color = Color.Black,
+            )
+        }
     }
 }

19cb450ea656612963ff6f7cbe66e8337406b414
DwEnn <prgohn@gmail.com>
2024-07-08 00:24:00 +0900

    1.0.2

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,10 +1,10 @@
 object AppConfig {
     const val minSdkVersion = 24
-    const val targetSdkVersion = 33
-    const val compileSdkVersion = 33
+    const val targetSdkVersion = 34
+    const val compileSdkVersion = 34
 
-    const val versionCode = 5
-    const val versionName = "1.0.1"
+    const val versionCode = 6
+    const val versionName = "1.0.2"
 
     const val androidTestInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules =  "consumer-rules.pro"
